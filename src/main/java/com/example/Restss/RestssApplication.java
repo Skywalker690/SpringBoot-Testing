5d49f0c66de19b684449f73c8c9ef01c1bde2c0d
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-07 20:19:09 +0530

    Initial commit

---
+++ src/main/java/com/example/Restss/RestssApplication.java
@@ -0,0 +1,13 @@
+package com.example.Restss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Restss/RestssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Restss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
