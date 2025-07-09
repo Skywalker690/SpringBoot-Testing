5d86fac92dd5a48f4c5794f416cbffbacc5a8870
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-09 17:52:37 +0530

    Implemented Spring Validations(Last name should not be empty)

--- src/main/java/com/example/Restss/controller/StudentController.java
+++ src/main/java/com/example/Restss/controller/StudentController.java
@@ -5,9 +5,14 @@ import com.example.Restss.dto.StudentDto;
 import com.example.Restss.dto.StudentResponseDto;
 import com.example.Restss.model.Student;
 import com.example.Restss.service.StudentService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,7 +25,7 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public StudentResponseDto SaveStudent(@RequestBody StudentDto dto){
+    public StudentResponseDto SaveStudent(@Valid @RequestBody StudentDto dto){
         return this.studentService.SaveStudent(dto);
     }
 
@@ -44,5 +49,18 @@ public class StudentController {
     @ResponseStatus(HttpStatus.OK)
     public String deleteStudent(@PathVariable int studentId){
         return this.studentService.deleteStudent(studentId);
+    }
+
+    //Last name should not be empty
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exp){
+        var errors = new HashMap<String,String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError)error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName,errorMessage);
+                });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/Restss/dto/StudentDto.java
+++ src/main/java/com/example/Restss/dto/StudentDto.java
@@ -1,7 +1,11 @@
 package com.example.Restss.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record StudentDto(
+        @NotNull
         String firstname,
+        @NotNull
         String lastname,
         String email,
         Integer schoolId
